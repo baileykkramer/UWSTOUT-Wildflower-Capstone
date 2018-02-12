98e5f72b6c42451011957c3fd80f44155a871c7d
d'Artagnan Kramer <kramerd8425@my.uwstout.edu>
2018-02-12 14:38:22 -0600

    Finished three-digit classification

--- src/app/three-digit/three-digit.component.ts
+++ src/app/three-digit/three-digit.component.ts
@@ -18,12 +18,27 @@ export class ThreeDigitComponent implements OnInit {
     { type: '5 Regular Parts', code: 5},
     { type: '6 Regular Parts', code: 6},
     { type: '7 or More Regular Parts', code: 7},
-    { type: 'Parts Indistinguishable', code: 8}
+    { type: 'Parts Indistinguishable', code: 8},
+    {type: 'Goldenrod', code:9},
+    {type: 'Aster', code:9.01}
   ];
 
-  plantType = [];
+  plantType = 
+  [
+    {type: 'No Apparent Leaves', code: 1},
+    {type: 'Basal Leaves Only', code: 2},
+    {type: 'Alternate Leaves', code: 3},
+    {type: 'Opposite or Whorled Leaves', code: 4},
+    {type: 'Shrubs', code: 5},
+    {type: 'Vines', code: 6},
+  ];
 
-  leafType = [];
+  leafType = [
+    {type: 'No apparent Leaves', code: 1},
+    {type: 'Leaves Entire', code: 2},
+    {type: 'Leaves Toothed or Lobed', code: 3},
+    {type: 'Leaves Divided', code: 4},
+  ];
 
   constructor() { }
 
@@ -34,13 +49,24 @@ export class ThreeDigitComponent implements OnInit {
 
   select(num) {
     if (this.stage === 1) {
-      this.finalNum += num * 100;
+      if(num >= 9)
+      {
+         //This wil bring Goldenrod and Aster to a three digt number
+        this.finalNum += num * 100; 
+        // Then bring the stage to 4 and exit
+        this.stage = 4;
+        console.log(this.finalNum, this.stage);
+      }
+      else
+      {
+      this.finalNum += num * 100; //Brings the flower type to hundreds position 
+      }
     }
     if (this.stage === 2) {
-      this.finalNum += num * 10;
+      this.finalNum += num * 10; // Brings the plant type to the tens position
     }
     if (this.stage === 3) {
-      this.finalNum += num * 1;
+      this.finalNum += num; //Technically "num * 1", also sets leaf type to last digit
     }
     this.stage++;
     console.log(this.finalNum, this.stage);
