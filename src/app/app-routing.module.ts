194bfd5c570a845ccf67e9c4dff75eb64c4310e1
d'Artagnan Kramer <kramerd8425@my.uwstout.edu>
2018-02-19 15:03:59 -0600

    Added Help page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClassificationComponent } from './classification/classification.component';
 import { GlosseryComponent } from './glossery/glossery.component';
 import { GallaryComponent } from './gallary/gallary.component';
+import { HelpComponent } from './help/help.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/classification', pathMatch: 'full' },
   { path: 'classification', component: ClassificationComponent },
   { path: 'glossery', component: GlosseryComponent },
-  { path: 'gallary', component: GallaryComponent }
+  { path: 'gallary', component: GallaryComponent },
+  { path: 'help', component: HelpComponent}
 ];
 
 @NgModule({
