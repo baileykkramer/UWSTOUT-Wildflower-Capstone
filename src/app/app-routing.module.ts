474776f72e756ca17cf8d4380538cbd827f2d325
Justin Ellefson <ellefsonj0224@my.uwstout.edu>
2018-03-05 13:59:25 -0600

    Removal of Glossary Option for Resources

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClassificationComponent } from './classification/classification.component';
 import { GlosseryComponent } from './glossery/glossery.component';
-import { GallaryComponent } from './gallary/gallary.component';
 import { HelpComponent } from './help/help.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/classification', pathMatch: 'full' },
   { path: 'classification', component: ClassificationComponent },
   { path: 'glossery', component: GlosseryComponent },
-  { path: 'gallary', component: GallaryComponent },
-  { path: 'help', component: HelpComponent}
+  { path: 'help', component: HelpComponent},
+  { path: 'resources', component: ResourcesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ClassificationComponent } from './classification/classification.component';
 import { GlosseryComponent } from './glossery/glossery.component';
-import { GallaryComponent } from './gallary/gallary.component';
 import { ThreeDigitComponent } from './three-digit/three-digit.component';
 import { HelpComponent } from './help/help.component';
 import { QuestionGenerateComponent } from './question-generate/question-generate.component';
 import { PHPService } from './php-service.service';
 import { Http, HttpModule } from '@angular/http';
+import { ResourcesComponent } from './resources/resources.component';
 
 
 @NgModule({
@@ -19,10 +19,10 @@ import { Http, HttpModule } from '@angular/http';
     AppComponent,
     ClassificationComponent,
     GlosseryComponent,
-    GallaryComponent,
     ThreeDigitComponent,
     HelpComponent,
-    QuestionGenerateComponent
+    QuestionGenerateComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
