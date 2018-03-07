4a467676094d758bab6cdadec529f1a0f919e0d4
d'Artagnan Kramer <kramerd8425@my.uwstout.edu>
2018-03-07 14:56:34 -0600

    Filled out the rest of the 'Help' page, site title is Wildflower guide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Newcombs Wildflower Guide';
+  title = 'Wildflower Guide';
   homeButton(): void {
     window.location.reload();
   }
