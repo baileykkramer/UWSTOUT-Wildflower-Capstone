765d107aef2481c0488cc3f6abdae2483846d177
d'Artagnan Kramer <kramerd8425@my.uwstout.edu>
2018-03-19 13:35:52 -0500

    Removed Newcomb's from title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,10 @@ import { PHPService } from './php-service.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'Wildflower Guide';
-=======
-  title = 'Newcombs Wildflower Guide';
 
   constructor(private php: PHPService) { }
 
->>>>>>> 474776f72e756ca17cf8d4380538cbd827f2d325
   homeButton(): void {
     window.location.reload();
   }
