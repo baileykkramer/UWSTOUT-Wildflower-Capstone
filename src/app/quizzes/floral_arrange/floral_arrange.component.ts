c92c19c57f97a2fd096d77944370660ae45edd80
d'Artagnan Kramer <kramerd8425@my.uwstout.edu>
2018-04-26 14:08:21 -0500

    updating the my version

--- src/app/quizzes/floral_arrange/floral_arrange.component.ts
+++ src/app/quizzes/floral_arrange/floral_arrange.component.ts
@@ -8,145 +8,141 @@ import { Component, OnInit } from '@angular/core';
 export class Floral_Arrange implements OnInit {
 
   stage: number;
-  answers: number [];
-  instructions = 'Please select the answer that best describes your specimen';
+  answers: number[];
+  anstype: string[];
+  instructions = 'Question ' + 1 + ' of 10';
   errorMsg = 'The code you have entered does not corresopond to any plant in our database. Please reexamine your entries and try again.';
-  stage1Title = 'Question One';
-  stage2Title = 'Question Two';
-  stage3Title = 'Question Three';
-  stage4Title = 'Question Four';
-  stage5Title = 'Question Five';
-  stage6Title = 'Question Six';
-  stage7Title = 'Question Seven';
-  stage8Title = 'Question Eight';
-  stage9Title = 'Question Nine';
-  stage10Title = 'Question Ten';
+  stage1Title = 'What is an elongated flower cluster with stalked flowers arranged along a central stem called?';
+  stage2Title = 'What is an elongate flower cluster with stalkless flowers arranged along a centaral stem called?';
+  stage3Title = 'What is an elongated branched flower cluster called?';
+  stage4Title = 'A more or less flat-topped, branched flower cluster in which the branching is typically opposite is called what?';
+  stage5Title = 'What is a flat-topped or convex branched flower cluster in which the branching is typically alternate called?';
+  stage6Title = 'What is a flower cluster in which all the flower stalks radiate from the same point, like the ribs of an umbrella called?';
+  stage7Title = 'What is a group of flowers joined together in a short, dense, terminal cluster, as in the clovers and in all members of the Composite Family is called?';
+  stage8Title = 'What is the fleshy spike of flowers in a species of the Arum Family Called?';
+  stage9Title = 'A large bract enclosing a flower cluster, as in the Arum Family is called what?';
+  stage10Title = 'Arranged in a circle around a central point is called what?';
   stage11Title = 'Results';
   classificationTitle = 'Plant Classification';
   errorTitle = 'Classification Issue';
 
-  QuestionOne = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ];
+  //
+  QuestionOne =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionTwo = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ];
+  QuestionTwo =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionThree = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ];
+  QuestionThree =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionFour = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ]; 
+  QuestionFour =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionFive = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ];
+  QuestionFive =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionSix = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ];
+  QuestionSix =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionSeven = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ];
+  QuestionSeven =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionEight = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ];
+  QuestionEight =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionNine = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ];
+  QuestionNine =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionTen = 
-  [
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer A', code: 0},
-    { type: 'Wrong Answer B', code: 0},
-    { type: 'Wrong Answer C', code: 0},
-    { type: 'Wrong Answer D', code: 0},
-  ];
+  QuestionTen =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
   constructor() { }
 
-  ngOnInit() 
-  {
+  ngOnInit() {
     this.stage = 0;
     this.answers = [];
+    this.anstype = [];
   }
-  select(num) 
-  {
-        this.answers[this.stage] = num;
-        this.stage++;
-    console.log(' Code: ', this.answers[this.stage-1], 'Stage:', this.stage);
+  select(num, string) {
+    this.answers[this.stage] = num;
+    this.anstype[this.stage] = string;
+    this.stage++;
+    this.instructions = 'Question ' + (this.stage + 1) + ' of 10';
+    console.log('Type: ', this.anstype[this.stage - 1], ' Code: ', this.answers[this.stage - 1], 'Stage:', this.stage);
   }
 
-
-  CorrectAnswer = 
-  [
-    {type: 'Correct 1', code: 0},
-    {type: 'Correct 1', code: 1},
-    {type: 'Correct 1', code: 2},
-    {type: 'Correct 1', code: 3},
-    {type: 'Correct 1', code: 4},
-    {type: 'Correct 1', code: 5},
-    {type: 'Correct 1', code: 6},
-    {type: 'Correct 1', code: 7},
-    {type: 'Correct 1', code: 8},
-    {type: 'Correct 1', code: 9},
-  ];
-
-
-  display(num)
-  {
-  
-  }
+  CorrectAnswer =
+    [
+      { type: 'Raceme', code: 0 },
+      { type: 'Correct 2', code: 1 },
+      { type: 'Correct 3', code: 2 },
+      { type: 'Correct 4', code: 3 },
+      { type: 'Correct 5', code: 4 },
+      { type: 'Correct 6', code: 5 },
+      { type: 'Correct 7', code: 6 },
+      { type: 'Correct 8', code: 7 },
+      { type: 'Correct 9', code: 8 },
+      { type: 'Correct 10', code: 9 },
+    ];
 }
--- src/app/quizzes/floral_structure/floral_structure.component.ts
+++ src/app/quizzes/floral_structure/floral_structure.component.ts
@@ -7,111 +7,142 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Floral_Structure implements OnInit {
 
-  anwers: number[];
+  stage: number;
+  answers: number[];
+  anstype: string[];
+  instructions = 'Question ' + 1 + ' of 10';
+  errorMsg = 'The code you have entered does not corresopond to any plant in our database. Please reexamine your entries and try again.';
+  stage1Title = 'What is an elongated flower cluster with stalked flowers arranged along a central stem called?';
+  stage2Title = 'Question Two';
+  stage3Title = 'Question Three';
+  stage4Title = 'Question Four';
+  stage5Title = 'Question Five';
+  stage6Title = 'Question Six';
+  stage7Title = 'Question Seven';
+  stage8Title = 'Question Eight';
+  stage9Title = 'Question Nine';
+  stage10Title = 'Question Ten';
+  stage11Title = 'Results';
+  classificationTitle = 'Plant Classification';
+  errorTitle = 'Classification Issue';
 
+  //
+  QuestionOne =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionOne = 
-  [
-    {type: 'Question:', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ];
+  QuestionTwo =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionTwo = 
-  [
-    {type: 'Question:', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ];
+  QuestionThree =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionThree = 
-  [
-    {type: 'Question:', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ];
+  QuestionFour =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionFour = 
-  [
-    {type: 'Question:', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ]; 
+  QuestionFive =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionFive = 
-  [
-    {type: 'No apparent Leaves', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ];
+  QuestionSix =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionSix = 
-  [
-    {type: 'No apparent Leaves', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ];
+  QuestionSeven =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionSeven = 
-  [
-    {type: 'No apparent Leaves', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ];
+  QuestionEight =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionEight = 
-  [
-    {type: 'No apparent Leaves', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ];
+  QuestionNine =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
-  QuestionNine = 
-  [
-    {type: 'No apparent Leaves', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ];
-
-  QuestionTen = 
-  [
-    {type: 'No apparent Leaves', code: 0},
-    { type: 'Answer', code: 1},
-    { type: 'Wrong Answer', code: 2},
-    { type: 'Wrong Answer', code: 3},
-    { type: 'Wrong Answer', code: 4},
-    { type: 'Wrong Answer', code: 5},
-  ];
+  QuestionTen =
+    [
+      { type: 'Answer', code: 1 },
+      { type: 'Wrong Answer A', code: 0 },
+      { type: 'Wrong Answer B', code: 0 },
+      { type: 'Wrong Answer C', code: 0 },
+      { type: 'Wrong Answer D', code: 0 },
+    ];
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.stage = 0;
+    this.answers = [];
+    this.anstype = [];
+  }
+  select(num, string) {
+    this.answers[this.stage] = num;
+    this.anstype[this.stage] = string;
+    this.stage++;
+    this.instructions = 'Question ' + (this.stage + 1) + ' of 10';
+    console.log('Type: ', this.anstype[this.stage - 1], ' Code: ', this.answers[this.stage - 1], 'Stage:', this.stage);
+  }
 
+  CorrectAnswer =
+    [
+      { type: 'Raceme', code: 0 },
+      { type: 'Correct 2', code: 1 },
+      { type: 'Correct 3', code: 2 },
+      { type: 'Correct 4', code: 3 },
+      { type: 'Correct 5', code: 4 },
+      { type: 'Correct 6', code: 5 },
+      { type: 'Correct 7', code: 6 },
+      { type: 'Correct 8', code: 7 },
+      { type: 'Correct 9', code: 8 },
+      { type: 'Correct 10', code: 9 },
+    ];
 }
